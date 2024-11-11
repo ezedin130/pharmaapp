239782598ec1e713a599a2ce7880448d03c6c4e1
ezedin130/pharma <ezedinawol130@gmail.com>
2024-11-11 12:35:01 +0300

    new file:   lib/Images/decsappointment.png 	new file:   lib/Images/docsappointment.png 	new file:   lib/Images/healthcheckup.webp 	new file:   lib/Images/uppresc.png 	modified:   lib/Screens/HomeScreen.dart 	modified:   pubspec.yaml

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, must_be_immutable, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
 import 'package:pharma/Constants.dart';
@@ -22,7 +22,8 @@ class _HomeScreenState extends State<HomeScreen> {
             children: [
               Card(
                 margin: EdgeInsets.symmetric(
-                    horizontal: 10), // Adds padding to Card edges
+                  horizontal: 10.0,
+                ),
                 child: Padding(
                   padding: const EdgeInsets.all(8.0), // Padding inside the card
                   child: Row(
@@ -33,7 +34,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                       SizedBox(
                         width: 8.0,
-                      ), // Adds spacing between Icon and TextField
+                      ),
                       Expanded(
                         child: TextField(
                           decoration: textFieldDecoration.copyWith(
@@ -47,39 +48,62 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               SizedBox(
                 height: 16.0,
-              ), 
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ImageButtons(
+                      text: 'Upload prescription',
+                      imagetext: 'lib/Images/uppresc.png',
+                      width: 90.0,
+                    ),
+                    ImageButtons(
+                      text: 'Doctor Appointment',
+                      imagetext: 'lib/Images/docsappointment.png',
+                      width: 90.0,
+                    ),
+                    ImageButtons(
+                      text: 'Health Checkup',
+                      imagetext: 'lib/Images/healthcheckup.webp',
+                      width: 90.0,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              // Column(
+              //   children: [
+              //     Text(
+              //       'OUR BEST SELLING ITEMS',
+              //       style: TextStyle(color: Colors.blueAccent),
+              //     ),
+              //   ],
+              // ),
               Card(
                 child: Row(
                   children: [
                     NavigButtns(
                       text: 'Home',
-                      icon: Icon(
-                        Icons.home
-                      ),
+                      icon: Icon(Icons.home),
                     ),
                     NavigButtns(
                       text: 'Category',
-                      icon: Icon(
-                        Icons.category
-                      ),
+                      icon: Icon(Icons.category),
                     ),
                     NavigButtns(
                       text: 'Upload',
-                      icon: Icon(
-                        Icons.upload_file
-                      ),
+                      icon: Icon(Icons.upload_file),
                     ),
                     NavigButtns(
                       text: 'Profile',
-                      icon: Icon(
-                        Icons.account_circle_rounded
-                      ),
+                      icon: Icon(Icons.account_circle_rounded),
                     ),
                     NavigButtns(
                       text: 'Cart',
-                      icon: Icon(
-                        Icons.shopping_cart
-                      ),
+                      icon: Icon(Icons.shopping_cart),
                     ),
                   ],
                 ),
@@ -92,8 +116,43 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
+class ImageButtons extends StatelessWidget {
+  ImageButtons(
+      {required this.text, required this.imagetext, required this.width});
+
+  String text;
+  String imagetext;
+  double width;
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Container(
+        color: Color(0xffEFF3F8),
+        child: MaterialButton(
+          onPressed: () {},
+          child: Column(
+            children: [
+              Image.asset(
+                imagetext,
+                fit: BoxFit.fill,
+                width: 100.0,
+              ),
+              Text(
+                text,
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class NavigButtns extends StatelessWidget {
-   NavigButtns({required this.icon , required this.text});
+  NavigButtns({required this.icon, required this.text});
   String text;
   Icon icon;
 
@@ -102,15 +161,13 @@ class NavigButtns extends StatelessWidget {
     return Expanded(
       child: Column(
         children: [
-          Text(
-            text
-          ),
+          Text(text),
           IconButton(
-            onPressed: (){},
-            icon:icon,
+            onPressed: () {},
+            icon: icon,
           ),
         ],
       ),
-      );
+    );
   }
 }
