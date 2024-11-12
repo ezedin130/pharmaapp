142b6a52748858d31be94492b339343131006513
ezedin130/pharma <ezedinawol130@gmail.com>
2024-11-12 10:21:45 +0300

    modified:   lib/Screens/HomeScreen.dart

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -31,19 +31,16 @@ class _HomeScreenState extends State<HomeScreen> {
                         horizontal: 10.0,
                       ),
                       child: Padding(
-                        padding: const EdgeInsets.all(
-                          8.0,
-                        ), // Padding inside the card
+                        padding: const EdgeInsets.all(8.0),
                         child: Row(
                           children: [
                             Icon(
                               Icons.search,
                               color: Colors.blueGrey,
                             ),
-                            SizedBox(
-                              width: 8.0,
-                            ),
+                            SizedBox(width: 8.0),
                             Expanded(
+                              // Use Expanded here in Row to allow full-width TextField
                               child: TextField(
                                 decoration: textFieldDecoration.copyWith(
                                   hintText:
@@ -55,43 +52,36 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       ),
                     ),
-                    SizedBox(
-                      height: 16.0,
+                    SizedBox(height: 16.0),
+                    Row(
+                      children: [
+                        ImageButtons(
+                          text: 'Upload prescription',
+                          imagetext: 'lib/Images/uppresc.png',
+                        ),
+                        ImageButtons(
+                          text: 'Doctor Appointment',
+                          imagetext: 'lib/Images/docsappointment.png',
+                        ),
+                        ImageButtons(
+                          text: 'Health Checkup',
+                          imagetext: 'lib/Images/healthcheckup.webp',
+                        ),
+                      ],
                     ),
-                    Expanded(
-                      child: Row(
-                        children: [
-                          ImageButtons(
-                            text: 'Upload prescription',
-                            imagetext: 'lib/Images/uppresc.png',
+                    SizedBox(height: 16.0),
+                    Column(
+                      children: [
+                        Text(
+                          'OUR BEST SELLING ITEMS',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
                           ),
-                          ImageButtons(
-                            text: 'Doctor Appointment',
-                            imagetext: 'lib/Images/docsappointment.png',
-                          ),
-                          ImageButtons(
-                            text: 'Health Checkup',
-                            imagetext: 'lib/Images/healthcheckup.webp',
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 16.0,
-                    ),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          Text(
-                            'OUR BEST SELLING ITEMS',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Row(
-                            children: [
+                        ),
+                        Row(
+                          children: [
                             ImageButtons(
                               text: 'Creatine',
                               imagetext: 'lib/Images/1.jpg',
@@ -108,108 +98,103 @@ class _HomeScreenState extends State<HomeScreen> {
                               text: 'Whey Protein',
                               imagetext: 'lib/Images/4.jpg',
                             ),
-                          ]),
-                          Row(
-                            children: [
-                              ImageButtons(
-                                text: 'Probiotics',
-                                imagetext: 'lib/Images/5.jpg',
-                              ),
-                              ImageButtons(
-                                text: 'Baby food',
-                                imagetext: 'lib/Images/6.jpg',
-                              ),
-                              ImageButtons(
-                                text: 'Sexual Welness',
-                                imagetext: 'lib/Images/7.jpg',
-                              ),
-                              ImageButtons(
-                                text: 'Baby Diapers',
-                                imagetext: 'lib/Images/8.jpg',
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              ImageButtons(
-                                text: 'BP monitors',
-                                imagetext: 'lib/Images/9.jpg',
-                              ),
-                              ImageButtons(
-                                text: 'Moisturizers',
-                                imagetext: 'lib/Images/10.jpg',
-                              ),
-                              ImageButtons(
-                                text: 'Nebulizer',
-                                imagetext: 'lib/Images/11.jpg',
-                              ),
-                              ImageButtons(
-                                text: 'Collagen',
-                                imagetext: 'lib/Images/12.jpg',
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 16.0,
-                    ),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          Text(
-                            'Most Searched Items',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            ImageButtons(
+                              text: 'Probiotics',
+                              imagetext: 'lib/Images/5.jpg',
                             ),
+                            ImageButtons(
+                              text: 'Baby food',
+                              imagetext: 'lib/Images/6.jpg',
+                            ),
+                            ImageButtons(
+                              text: 'Sexual Wellness',
+                              imagetext: 'lib/Images/7.jpg',
+                            ),
+                            ImageButtons(
+                              text: 'Baby Diapers',
+                              imagetext: 'lib/Images/8.jpg',
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            ImageButtons(
+                              text: 'BP monitors',
+                              imagetext: 'lib/Images/9.jpg',
+                            ),
+                            ImageButtons(
+                              text: 'Moisturizers',
+                              imagetext: 'lib/Images/10.jpg',
+                            ),
+                            ImageButtons(
+                              text: 'Nebulizer',
+                              imagetext: 'lib/Images/11.jpg',
+                            ),
+                            ImageButtons(
+                              text: 'Collagen',
+                              imagetext: 'lib/Images/12.jpg',
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 16.0),
+                    Column(
+                      children: [
+                        Text(
+                          'Most Searched Items',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
                           ),
-                          Row(
-                            children: [
-                              ImageButtons(
-                                text: 'Collagen',
-                                imagetext: 'lib/Images/12.jpg',
-                              ),
-                              ImageButtons(
-                                text: 'BP monitors',
-                                imagetext: 'lib/Images/9.jpg',
-                              ),
-                              ImageButtons(
-                                text: 'Baby food',
-                                imagetext: 'lib/Images/6.jpg',
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              ImageButtons(
-                                text: 'Nebulizer',
-                                imagetext: 'lib/Images/11.jpg',
-                              ),
-                              ImageButtons(
-                                text: 'Baby Diapers',
-                                imagetext: 'lib/Images/8.jpg',
-                              ),
-                              ImageButtons(
-                                text: 'Whey Protein',
-                                imagetext: 'lib/Images/4.jpg',
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
+                        ),
+                        Row(
+                          children: [
+                            ImageButtons(
+                              text: 'Collagen',
+                              imagetext: 'lib/Images/12.jpg',
+                            ),
+                            ImageButtons(
+                              text: 'BP monitors',
+                              imagetext: 'lib/Images/9.jpg',
+                            ),
+                            ImageButtons(
+                              text: 'Baby food',
+                              imagetext: 'lib/Images/6.jpg',
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            ImageButtons(
+                              text: 'Nebulizer',
+                              imagetext: 'lib/Images/11.jpg',
+                            ),
+                            ImageButtons(
+                              text: 'Baby Diapers',
+                              imagetext: 'lib/Images/8.jpg',
+                            ),
+                            ImageButtons(
+                              text: 'Whey Protein',
+                              imagetext: 'lib/Images/4.jpg',
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ],
                 ),
               ),
             ),
-            FooterCard()
+            FooterCard(),
           ],
         ),
       ),
     );
   }
 }
-
